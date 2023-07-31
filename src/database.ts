2dc23012c5aec903b8b139c727205ff44b4a9d30
Zulfu Yilmaz <z.yilmaz08@gmail.com>
2023-07-31 23:09:30 +0100

    Revert "Backend initial commit: Installed necessary dependencies, including postgres and created a skeleton structure"
    This reverts commit 5cf466c25458498793036b1b03f71949ce68fc9d.

--- src/database.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('dbname', 'dbuser', 'dbpassword', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-export default sequelize;
