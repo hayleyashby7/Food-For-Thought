5b95100d857a33e002b7723cfbbac0c47ffc19cd
Fatima Zahra <fathima_z@yahoo.com>
2023-08-01 17:52:30 +0100

    feat: created a skeletal compnt struc for landg pg

---
+++ client/src/components/not_found/not_found.tsx
@@ -0,0 +1,11 @@
+import Image from "..//../assets/images/404Oops.png";
+
+const NotFound = () => (
+  <>
+    <img src={Image} alt="404 page not found" />
+    <h2>404 - Page Not Found!</h2>
+    <p>Sorry! Please go back to the homepage and try again</p>
+  </>
+);
+
+export default NotFound;
