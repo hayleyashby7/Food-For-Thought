d4c8eccddb343b95ce72896dd64c83fc994d412d
Fatima Zahra <fathima_z@yahoo.com>
2023-08-03 18:44:57 +0100

    fix: tidied up css files+added Spclogo onto footer

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -1,8 +1,12 @@
+import React from "react";
+import Spoonacularlogo from "../../assets/images/SpoonacularLogo1.png";
+
 const Footer: React.FC = () => (
-  <footer className="bg-yellow-400 text-red-900 flex items-center justify-center">
-    <p>
+  <footer className="bg-yellow-400 text-green-900 flex items-center justify-center">
+    <div className="flex items-center">
+      <img src={Spoonacularlogo} alt="Spoonacular Logo" className="h-12 mr-4" />
       <strong>@ Food For Thought 2023</strong>
-    </p>
+    </div>
   </footer>
 );
 
