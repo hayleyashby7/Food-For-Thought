ab4e2ee2e3a4ad61862f573f323ebbcd9b3d3ca1
Fatima Zahra <fathima_z@yahoo.com>
2023-08-03 18:44:57 +0100

    fix: tidied up css files+added Spclogo onto footer

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,6 +1,4 @@
 import Nav from "./nav";
-import "typeface-bungee";
-import Spoonacularlogo from "../../assets/images/SpoonacularLogo1.png";
 import FFTLogo from "../../assets/images/logo.png";
 
 const Header: React.FC = () => (
@@ -9,11 +7,6 @@ const Header: React.FC = () => (
       src={FFTLogo}
       alt="Food For Thought Logo"
       className="block mx-auto h-24 m-2"
-    />
-    <img
-      src={Spoonacularlogo}
-      alt="Spoonacular Logo"
-      className="block mx-auto h-24"
     />
     <Nav />
   </header>
