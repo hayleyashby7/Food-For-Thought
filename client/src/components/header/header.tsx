555207c01ebe303fd2b50a5450e39c192285a783
Fatima Zahra <fathima_z@yahoo.com>
2023-08-03 01:11:08 +0100

    feat: created a logo for our FoodForThought app

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,13 +1,18 @@
 import Nav from "./nav";
 import "typeface-bungee";
-import logo from "../../assets/images/SpoonacularLogo.png";
+import Spoonacularlogo from "../../assets/images/SpoonacularLogo1.png";
+import FFTLogo from "../../assets/images/logo.png";
 
 const Header: React.FC = () => (
   <header className="bg-yellow-400 text-green-900 border border-black p-2 font-bold w-full flex items-center justify-center box-border">
-    <h1 className="font-bungee text-4xl">Food for Thought</h1>
     <img
-      src={logo}
-      alt="Images of different dishes"
+      src={FFTLogo}
+      alt="Food For Thought Logo"
+      className="block mx-auto h-24 m-2"
+    />
+    <img
+      src={Spoonacularlogo}
+      alt="Spoonacular Logo"
       className="block mx-auto h-24"
     />
     <Nav />
