13f720a85f473d6bfb8179e7cbe189ab99a5c4a4
Ahmed <imaneben8@gmail.com>
2023-08-04 12:59:01 +0100

    feat:updates the router to startbutton

--- client/src/components/start_planning/StartButton.tsx
+++ client/src/components/start_planning/StartButton.tsx
@@ -8,12 +8,14 @@ interface ButtonProps {
 
 const StartButton: React.FC<ButtonProps> = ({ onClick, label, className }) => {
   return (
-    <button
-      className={`px-20 py-10 font-semibold rounded-md text-white bg-orange-500 hover:bg-orange-600 ${className}`}
-      onClick={onClick}
-    >
-      {label}
-    </button>
+    <div className="flex justify-center items-center h-screen">
+      <button
+        className={`px-20 py-10 font-semibold rounded-md  text-white bg-orange-500 hover:bg-orange-600 ${className}`}
+        onClick={onClick}
+      >
+        {label}
+      </button>
+    </div>
   );
 };
 
