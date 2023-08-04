7d22a6ba4086a39a347f9179616d9c61d3ddda3f
Fatima Zahra <fathima_z@yahoo.com>
2023-08-04 02:06:48 +0100

    feat: Added gradient title in header.tsx

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -5,7 +5,10 @@ const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center bg-yellow-100 text-red-900 w-full h-96">
       <p className="text-center text-2xl">
-        <strong>Welcome to our Food For Thought App!</strong>
+        <strong>
+          Welcome to our{" "}
+          <span className="gradient-text text-2xl">Food For Thought</span> App!
+        </strong>
       </p>
       <br />
       <p className="text-center text-2xl">
