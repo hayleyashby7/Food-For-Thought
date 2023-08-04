ddd84fbd8b268bf9a39f70001066f583fddffeae
Ahmed <imaneben8@gmail.com>
2023-08-04 12:59:01 +0100

    feat:updates the router to startbutton

--- client/src/components/router/router.tsx
+++ client/src/components/router/router.tsx
@@ -3,6 +3,7 @@ import Layout from "../layout/layout";
 import Home from "../home/home";
 import StartPlanning from "../start_planning/start_planning";
 import NotFound from "../not_found/not_found";
+import StartButton from "../start_planning/StartButton";
 
 export const Router: React.FC = () => {
   return (
@@ -11,6 +12,17 @@ export const Router: React.FC = () => {
         <Route index element={<Home />} />
         <Route path="startplanning" element={<StartPlanning />} />
         <Route path="*" element={<NotFound />} />
+        <Route
+          path="startbutton"
+          element={
+            <StartButton
+              onClick={function (): void {
+                throw new Error("Function not implemented.");
+              }}
+              label={"Start Planning My Meal"}
+            />
+          }
+        />
       </Route>
     </Routes>
   );
