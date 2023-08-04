70d20195a322da883ae77a077c26b0ced3f1357a
Fatima Zahra <fathima_z@yahoo.com>
2023-08-04 23:09:24 +0100

    test: implemented tests for not_found &they passed

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-
 const Footer: React.FC = () => (
-  <footer className="bg-yellow-400 text-green-800 flex items-center justify-center p-4 text-center text-xs sm:text-sm md:text-base">
+  <footer className="bg-yellow-400 text-green-800 flex items-center justify-center p-4 text-center text-xs sm:text-sm md:text-base fixed bottom-0 inset-x-0">
     <div className="flex items-center">
       <strong>@ Food For Thought 2023</strong>
     </div>
