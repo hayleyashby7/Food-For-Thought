c212a8cbce0b57eb2567a821f3d25897fb9f7724
Ahmed <imaneben8@gmail.com>
2023-08-05 22:00:09 +0100

    feat: integration of the API to list diet options

--- client/src/components/start_planning/DietInput.tsx
+++ client/src/components/start_planning/DietInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface DietInputProps {
@@ -15,52 +15,36 @@ const DietInput: React.FC<DietInputProps> = ({ onDietSelect }) => {
     navigate("/removeingredient");
   };
 
+  const [dietOptions, setdietOptions] = useState(["apple", "lejuice", "foo"]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("localhost");
+      // data.json()
+      setdietOptions(data);
+    };
+
+    fetchData();
+  });
+
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-2xl font-semibold mb-4 ">
         Please Choose Your Type of Diet:
       </h2>
       <div className="flex flex-col space-y-4">
-        <button
-          className={`px-4 py-2 rounded-md ${
-            selectedDiet === "vegetarian"
-              ? "bg-orange-500 text-white"
-              : "bg-gray-200 text-black"
-          }`}
-          onClick={() => handleDietSelect("vegetarian")}
-        >
-          Vegetarian
-        </button>
-        <button
-          className={`px-4 py-2 rounded-md ${
-            selectedDiet === "vegan"
-              ? "bg-orange-500 text-white"
-              : "bg-gray-200 text-black"
-          }`}
-          onClick={() => handleDietSelect("vegan")}
-        >
-          Vegan
-        </button>
-        <button
-          className={`px-4 py-2 rounded-md ${
-            selectedDiet === "gluten-free"
-              ? "bg-orange-500 text-white"
-              : "bg-gray-200 text-black"
-          }`}
-          onClick={() => handleDietSelect("gluten-free")}
-        >
-          Gluten-Free
-        </button>
-        <button
-          className={`px-4 py-2 rounded-md ${
-            selectedDiet === "omnivore"
-              ? "bg-orange-500 text-white"
-              : "bg-gray-200 text-black"
-          }`}
-          onClick={() => handleDietSelect("omnivore")}
-        >
-          Omnivore
-        </button>
+        {dietOptions.map((dietOption) => (
+          <button
+            className={`px-4 py-2 rounded-md ${
+              selectedDiet === "vegetarian"
+                ? "bg-orange-500 text-white"
+                : "bg-gray-200 text-black"
+            }`}
+            onClick={() => handleDietSelect(dietOption)}
+          >
+            {dietOption}
+          </button>
+        ))}
       </div>
     </div>
   );
