1416e6a0fed45e9c55f2ca0b4e9a5927ec31244f
Fatima Zahra <fathima_z@yahoo.com>
2023-08-05 01:29:07 +0100

    test: implemented tests for header+nav&all passed

---
+++ client/src/components/header/nav.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Nav from "./nav";
+
+describe("Nav", () => {
+  it("renders the Nav component", () => {
+    const { container } = render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    );
+    expect(container).toBeInTheDocument();
+  });
+
+  it("displays nav items", () => {
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    );
+    const homeLink = screen.getByText("Home");
+    const startPlanningLink = screen.getByText("Start Planning");
+    expect(homeLink).toBeInTheDocument();
+    expect(startPlanningLink).toBeInTheDocument();
+  });
+
+  it("links point to the correct location", () => {
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    );
+    expect(screen.getByText("Home")).toHaveAttribute("href", "/");
+    expect(screen.getByText("Start Planning")).toHaveAttribute(
+      "href",
+      "/startplanning"
+    );
+  });
+});
