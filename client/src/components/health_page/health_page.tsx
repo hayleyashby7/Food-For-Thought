e78d7ed8fa91e9ca98ee5b66876ba5a483589e2f
Fatima Zahra <fathima_z@yahoo.com>
2023-08-05 11:45:20 +0100

    fix: made health page responsive

--- client/src/components/health_page/health_page.tsx
+++ client/src/components/health_page/health_page.tsx
@@ -24,17 +24,19 @@ const HealthPage = () => {
 
   return (
     <div className="min-h-screen flex justify-center bg-yellow-100">
-      <div className="h-48 w-64 p-6 bg-yellow-500 shadow-lg rounded-lg">
-        <h1 className="text-3xl font-bold mb-2 text-red-800">Health Page</h1>
-        <p className="font bold text-2x1  text-red-800">
+      <div className="w-64 h-48 sm:w-1/2 lg:w-1/3 p-6 bg-yellow-500 shadow-lg rounded-lg flex flex-col items-center sm:items-start">
+        <h1 className="text-lg sm:text-base md:text-lg lg:text-xl font-bold mb-2 text-red-800 text-center sm:text-left">
+          Health Page
+        </h1>
+        <p className="font-bold text-2x1  text-red-800 text-center sm:text-left">
           Status of Spoonacular API:
         </p>
         <br />
         <p
           className={
             apiStatus.startsWith("The API is up")
-              ? "text-green-800 font-bold"
-              : "text-red-600 font-bold"
+              ? "text-green-800 font-bold text-center sm:text-left"
+              : "text-red-600 font-bold text-center sm:text-left"
           }
         >
           {apiStatus}
