c3196ca8434c52976e2b89badc1a93448ab867ab
Ahmed <imaneben8@gmail.com>
2023-08-06 00:56:31 +0100

    update: updated the router

--- client/src/components/router/router.tsx
+++ client/src/components/router/router.tsx
@@ -37,8 +37,8 @@ export const Router: React.FC = () => {
           path="dietinput"
           element={<DietInput onDietSelect={() => ""} />}
         />
+        <Route path="removeingredient" element={<RemoveIngredient />} />
       </Route>
-      <Route path="removeingredient" element={<RemoveIngredient />} />
     </Routes>
   );
 };
