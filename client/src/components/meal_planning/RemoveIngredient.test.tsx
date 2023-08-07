fab965b4624895751aca28493b8bc3fecb39dcc4
Ahmed <imaneben8@gmail.com>
2023-08-07 22:32:55 +0100

    test: updated RemoveIngredient testing

--- client/src/components/meal_planning/RemoveIngredient.test.tsx
+++ client/src/components/meal_planning/RemoveIngredient.test.tsx
@@ -1,7 +1,5 @@
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-
-// Import the component
 import RemoveIngredient from "./RemoveIngredient";
 
 test("renders component with provided ingredient and triggers inputChanged on input", () => {
@@ -9,17 +7,10 @@ test("renders component with provided ingredient and triggers inputChanged on in
 
   const ingredient = "Test Ingredient";
 
-  // Render the component
   const { getByLabelText } = render(
     <RemoveIngredient ingredient={ingredient} inputChanged={mockInputChanged} />
   );
-
-  // Get the input element by its label text
   const inputElement = getByLabelText("Remove these ingredients");
-
-  // Simulate typing a value into the input
   fireEvent.input(inputElement, { target: { value: "New Value" } });
-
-  // Check if inputChanged function was called with the correct arguments
   expect(mockInputChanged).toHaveBeenCalledWith("New Value", true);
 });
