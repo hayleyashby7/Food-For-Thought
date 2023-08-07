2e57b5913958f8dac2243113886c563f9990eb42
Hayley Ashby <hayleyashby7@gmail.com>
2023-08-07 14:48:40 +0100

    feat: :sparkles: Meal plan form requests information and passes it to generator

--- client/src/components/meal_planning/MealPlanForm.tsx
+++ client/src/components/meal_planning/MealPlanForm.tsx
@@ -4,6 +4,7 @@ import RemoveIngredient from './RemoveIngredient';
 import SubmitButton from '../form_inputs/SubmitButton';
 import { useState } from 'react';
 import { MealPlanRequest } from '../../types/mealplan_request.type';
+import { MealPlanResponse } from '../../types/mealplan_response.type';
 
 interface InputsValid {
 	calories: boolean;
@@ -14,41 +15,32 @@ interface InputsValid {
 interface MealFormProps {
 	mealRequest: MealPlanRequest;
 	setMealRequest: (field: string, value: string) => void;
-	setMealResponse: (data: string) => void;
+	setMealResponse: (data: MealPlanResponse) => void;
 }
 
 export const MealForm: React.FC<MealFormProps> = ({ mealRequest, setMealRequest, setMealResponse }) => {
-	const [inputsValid, setInputsValid] = useState<InputsValid>({ calories: false, diet: false, remove: false });
+	const [inputsValid, setInputsValid] = useState<InputsValid>({ calories: false, diet: true, remove: false });
 
-	function onSubmit(event: React.FormEvent<HTMLFormElement>): void {
+	function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
 		event.preventDefault();
-		const form = event.currentTarget;
-		const data = new FormData(form);
-		console.log(data);
-		setMealResponse(data.toString());
 
-		/* useEffect(() => {
-			fetch('https://localhost:3000/api/mealplan?calories=2000&diet=vegetarian&exclude=shellfish')
-				.then((response) => {
-					if (!response.ok) {
-						throw new Error(`Server responded with ${response.status}`);
-					}
-					const contentType = response.headers.get('content-type');
-					if (!contentType || !contentType.includes('application/json')) {
-						throw new Error('Received non-JSON response from server.');
-					}
-					return response.json();
-				})
-				.then((data: MealPlanResponse) => {
-					setMeals(data.meals);
-					setNutrients(data.nutrients);
-					setLoading(false);
-				})
-				.catch((err: Error) => {
-					setError(err.message);
-					setLoading(false);
-				});
-		}, []); */
+		const fetchData = async () => {
+			try {
+				const response = await fetch('https://localhost:3000/api/mealplan?calories=2000&diet=vegetarian&exclude=shellfish');
+				if (!response.ok) {
+					throw new Error(`Server responded with ${response.status}`);
+				}
+				const contentType = response.headers.get('content-type');
+				if (!contentType || !contentType.includes('application/json')) {
+					throw new Error('Received non-JSON response from server.');
+				}
+				setMealResponse(await response.json());
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		fetchData();
 	}
 
 	const updateMealRequest = (field: string, value: string, valid: boolean) => {
@@ -57,11 +49,11 @@ export const MealForm: React.FC<MealFormProps> = ({ mealRequest, setMealRequest,
 	};
 
 	return (
-		<form method='post' onSubmit={onSubmit}>
+		<form method='post' onSubmit={handleSubmit}>
 			<CalorieInput calories={mealRequest.calories} inputChanged={(value, valid) => updateMealRequest('calories', value, valid)} />
 			{inputsValid.calories && <DietInput inputChanged={(value) => updateMealRequest('diet', value, true)} />}
-			{inputsValid.diet && <RemoveIngredient ingredient={mealRequest?.remove} inputChanged={(value, valid) => updateMealRequest('remove', value, valid)} />}
-			{inputsValid.diet && <SubmitButton text='Generate My Meals' />}
+			{inputsValid.calories && inputsValid.diet && <RemoveIngredient ingredient={mealRequest?.remove} inputChanged={(value, valid) => updateMealRequest('remove', value, valid)} />}
+			{inputsValid.calories && inputsValid.diet && <SubmitButton text='Generate My Meals' />}
 		</form>
 	);
 };
--- client/src/components/meal_planning/StartPlanning.tsx
+++ client/src/components/meal_planning/StartPlanning.tsx
@@ -3,21 +3,24 @@ import carrotWordCloud from '../../assets/images/CarrotWordCloud.jpg';
 import StartButton from '../form_inputs/StartButton';
 import MealForm from './MealPlanForm';
 import { MealPlanRequest } from '../../types/mealplan_request.type';
+import { MealPlanResponse } from '../../types/mealplan_response.type';
+import MealPlanGenerator from './MealPlanGenerator';
 
 const StartPlanning: React.FC = () => {
 	const [mealRequest, setMealRequest] = useState<MealPlanRequest>({ calories: '', diet: '', remove: '' });
 	const [formActive, setFormActive] = useState<boolean>(false);
-	const [mealResponse, setMealResponse] = useState('');
+	const [mealResponse, setMealResponse] = useState<MealPlanResponse>({ status: 0, meals: [], nutrients: { calories: 0, carbohydrates: 0, fat: 0, protein: 0 } });
 
 	const resetPlan = () => {
 		setMealRequest({ calories: '', diet: '', remove: '' });
-		setMealResponse('');
+		setMealResponse({ status: 0, meals: [], nutrients: { calories: 0, carbohydrates: 0, fat: 0, protein: 0 } });
 		setFormActive(false);
 	};
 
 	const startPlan = () => {
 		resetPlan();
 		setFormActive(true);
+		console.log(mealResponse);
 	};
 
 	return (
@@ -27,11 +30,10 @@ const StartPlanning: React.FC = () => {
 			</div>
 			<div className='flex-1 flex flex-col items-center bg-yellow-100 text-red-900 h-96 pl-30'>
 				{!formActive ? <StartButton onClick={startPlan} label='Start Plan' /> : <StartButton onClick={resetPlan} label='Reset Plan' />}
-				{formActive && !mealResponse ? (
+				{formActive && mealResponse.status === 0 && (
 					<MealForm mealRequest={mealRequest} setMealRequest={(field, value) => setMealRequest({ ...mealRequest, [field]: value })} setMealResponse={(data) => setMealResponse(data)} />
-				) : (
-					<p>{mealResponse}</p>
 				)}
+				{formActive && mealResponse.status !== 0 && <MealPlanGenerator mealResponse={mealResponse} />}
 			</div>
 		</div>
 	);
