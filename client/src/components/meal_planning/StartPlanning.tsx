057b834ef38564a9df9ec57b0b4b9ebd2e5ce357
Hayley Ashby <hayleyashby7@gmail.com>
2023-08-08 12:13:40 +0100

    style: :lipstick: Responsive styling given to meal planning form

--- client/src/components/meal_planning/StartPlanning.tsx
+++ client/src/components/meal_planning/StartPlanning.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import StartButton from "../form_inputs/StartButton";
-import MealForm from "./MealPlanForm";
-import { MealPlanRequest } from "../../types/mealplan_request.type";
-import { MealPlanResponse } from "../../types/mealplan_response.type";
-import MealPlanGenerator from "./MealPlanGenerator";
+import { useState } from 'react';
+import StartButton from '../form_inputs/StartButton';
+import MealForm from './MealPlanForm';
+import { MealPlanRequest } from '../../types/mealplan_request.type';
+import { MealPlanResponse } from '../../types/mealplan_response.type';
+import MealPlanGenerator from './MealPlanGenerator';
 
 const StartPlanning: React.FC = () => {
   const [mealRequest, setMealRequest] = useState<MealPlanRequest>({
@@ -28,38 +28,20 @@ const StartPlanning: React.FC = () => {
     setFormActive(false);
   };
 
-  const startPlan = () => {
-    resetPlan();
-    setFormActive(true);
-    console.log(mealResponse);
-  };
+	const startPlan = () => {
+		resetPlan();
+		setFormActive(true);
+	};
 
-  return (
-    <div className="flex flex-row min-h-screen bg-yellow-100">
-      <div className="flex-1 flex flex-col items-center text-red-900 h-96 pl-30">
-        {!formActive ? (
-          <StartButton onClick={startPlan} label="Start Plan" />
-        ) : (
-          <StartButton onClick={resetPlan} label="Reset Plan" />
-        )}
-        {formActive && mealResponse.status === 0 && (
-          <MealForm
-            mealRequest={mealRequest}
-            setMealRequest={(field, value) =>
-              setMealRequest({ ...mealRequest, [field]: value })
-            }
-            setMealResponse={(data) => setMealResponse(data)}
-          />
-        )}
-        {formActive && mealResponse.status !== 0 && (
-          <>
-            <br />
-            <MealPlanGenerator mealResponse={mealResponse} />
-          </>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex flex-col items-center bg-yellow-100 text-red-900'>
+			{!formActive ? <StartButton onClick={startPlan} label='Start Plan' /> : <StartButton onClick={resetPlan} label='Reset Plan' />}
+			{formActive && mealResponse.status === 0 && (
+				<MealForm mealRequest={mealRequest} setMealRequest={(field, value) => setMealRequest({ ...mealRequest, [field]: value })} setMealResponse={(data) => setMealResponse(data)} />
+			)}
+			{formActive && mealResponse.status !== 0 && <MealPlanGenerator mealResponse={mealResponse} />}
+		</div>
+	);
 };
 
 export default StartPlanning;
