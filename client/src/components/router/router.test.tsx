7efef53824bc7ad2f7d53b52fe5f288f1c78287d
Hayley Ashby <hayleyashby7@gmail.com>
2023-08-08 14:30:09 +0100

    test: :white_check_mark: Fix router test after label change

--- client/src/components/router/router.test.tsx
+++ client/src/components/router/router.test.tsx
@@ -1,34 +1,32 @@
-import { screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import Router from "./router";
-import { renderWithRouter } from "../../utils/test_utils";
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Router from './router';
+import { renderWithRouter } from '../../utils/test_utils';
 
-test("Router renders homepage", () => {
-  renderWithRouter(<Router />);
+test('Router renders homepage', () => {
+	renderWithRouter(<Router />);
 
-  const homeElement = screen.getByRole("main");
+	const homeElement = screen.getByRole('main');
 
-  expect(homeElement).toBeInTheDocument();
-  expect(homeElement.textContent).toContain("Welcome");
+	expect(homeElement).toBeInTheDocument();
+	expect(homeElement.textContent).toContain('Welcome');
 });
-test("Router renders startplanning page", () => {
-  renderWithRouter(<Router />, { route: "/startplanning" });
+test('Router renders startplanning page', () => {
+	renderWithRouter(<Router />, { route: '/startplanning' });
 
-  const startPlanningElement = screen.getByRole("main");
+	const startPlanningElement = screen.getByRole('main');
 
-  expect(startPlanningElement).toBeInTheDocument();
-  expect(startPlanningElement).toHaveTextContent("Start Planning");
+	expect(startPlanningElement).toBeInTheDocument();
+	expect(startPlanningElement).toHaveTextContent('Start Plan');
 });
-test("Router renders not found page", () => {
-  renderWithRouter(<Router />, { route: "/page-not-found" });
+test('Router renders not found page', () => {
+	renderWithRouter(<Router />, { route: '/page-not-found' });
 
-  const notFoundHeading = screen.getByRole("heading", { level: 2 });
-  const notFoundText = screen.getByText(/Sorry/i);
+	const notFoundHeading = screen.getByRole('heading', { level: 2 });
+	const notFoundText = screen.getByText(/Sorry/i);
 
-  expect(notFoundHeading).toBeInTheDocument();
-  expect(notFoundHeading).toHaveTextContent("404 - Page Not Found!");
-  expect(notFoundText).toBeInTheDocument();
-  expect(notFoundText).toHaveTextContent(
-    "Sorry! Please go back to the homepage and try again."
-  );
+	expect(notFoundHeading).toBeInTheDocument();
+	expect(notFoundHeading).toHaveTextContent('404 - Page Not Found!');
+	expect(notFoundText).toBeInTheDocument();
+	expect(notFoundText).toHaveTextContent('Sorry! Please go back to the homepage and try again.');
 });
