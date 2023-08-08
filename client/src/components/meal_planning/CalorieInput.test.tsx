08a730ab6db0804c6480cb950c7d951fb53d0de5
Hayley Ashby <hayleyashby7@gmail.com>
2023-08-08 13:16:51 +0100

    test: :bug: Fixed test bug with label name

--- client/src/components/meal_planning/CalorieInput.test.tsx
+++ client/src/components/meal_planning/CalorieInput.test.tsx
@@ -5,7 +5,7 @@ import CalorieInput from './CalorieInput';
 
 test('renders the input component', () => {
 	const { getByLabelText } = render(<CalorieInput calories='' inputChanged={() => {}} />);
-	const inputElement = getByLabelText('Please Enter Your Daily Calories');
+	const inputElement = getByLabelText('Daily Calories');
 	expect(inputElement).toBeInTheDocument();
 });
 
@@ -13,7 +13,7 @@ test('calls inputChanged function on input', () => {
 	const mockInputChanged = jest.fn();
 	const { getByLabelText } = render(<CalorieInput calories='' inputChanged={mockInputChanged} />);
 
-	const inputElement = getByLabelText('Please Enter Your Daily Calories');
+	const inputElement = getByLabelText('Daily Calories');
 	fireEvent.change(inputElement, { target: { value: '2000' } });
 
 	expect(mockInputChanged).toHaveBeenCalledWith('2000', true);
@@ -22,7 +22,7 @@ test('calls inputChanged function on input', () => {
 test('displays an error message for invalid input', () => {
 	const { getByLabelText, getByText } = render(<CalorieInput calories='' inputChanged={() => {}} />);
 
-	const inputElement = getByLabelText('Please Enter Your Daily Calories');
+	const inputElement = getByLabelText('Daily Calories');
 	fireEvent.change(inputElement, { target: { value: 'invalid' } });
 
 	const errorMessage = getByText('Only numbers allowed');
