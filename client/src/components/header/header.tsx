6091b94fabfb863efa1828c5abf33636754f134b
Hayley Ashby <hayleyashby7@gmail.com>
2023-08-09 15:41:34 +0100

    fix: :lipstick: Small header fixed

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -2,7 +2,7 @@ import Nav from './nav';
 import FFTLogo from '../../assets/images/logo.png';
 
 const Header: React.FC = () => (
-	<header className='bg-yellow-400 text-green-800 border border-black p-2 font-bold w-screen min-w-fit flex flex-col sm:flex-row justify-start box-border'>
+	<header className='bg-yellow-400 text-green-800 border border-black p-2 font-bold min-w-screen flex flex-col sm:flex-row justify-start box-border'>
 		<div className='flex flex-col sm:flex-row items-center'>
 			<img src={FFTLogo} alt='Food For Thought Logo' className='mx-auto h-24 md:h-20 m-2' />
 			<h1 className='p-2 m-2 gradient-text text-xl md:text-2xl text-center'>
