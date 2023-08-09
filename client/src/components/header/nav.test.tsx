3aee9387da91f42bb324704c11a100c96539b700
Hayley Ashby <hayleyashby7@gmail.com>
2023-08-09 11:52:39 +0100

    fix: :white_check_mark: Update tests to reflect changes to header and homepage

--- client/src/components/header/nav.test.tsx
+++ client/src/components/header/nav.test.tsx
@@ -1,40 +1,37 @@
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
-import Nav from "./nav";
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Nav from './nav';
 
-describe("Nav", () => {
-  it("renders the Nav component", () => {
-    const { container } = render(
-      <MemoryRouter>
-        <Nav />
-      </MemoryRouter>
-    );
-    expect(container).toBeInTheDocument();
-  });
+describe('Nav', () => {
+	it('renders the Nav component', () => {
+		const { container } = render(
+			<MemoryRouter>
+				<Nav />
+			</MemoryRouter>
+		);
+		expect(container).toBeInTheDocument();
+	});
 
-  it("displays nav items", () => {
-    render(
-      <MemoryRouter>
-        <Nav />
-      </MemoryRouter>
-    );
-    const homeLink = screen.getByText("Home");
-    const startPlanningLink = screen.getByText("Start Planning");
-    expect(homeLink).toBeInTheDocument();
-    expect(startPlanningLink).toBeInTheDocument();
-  });
+	it('displays nav items', () => {
+		render(
+			<MemoryRouter>
+				<Nav />
+			</MemoryRouter>
+		);
+		const homeLink = screen.getByText('Home');
+		const startPlanningLink = screen.getByText('Start Plan');
+		expect(homeLink).toBeInTheDocument();
+		expect(startPlanningLink).toBeInTheDocument();
+	});
 
-  it("links point to the correct location", () => {
-    render(
-      <MemoryRouter>
-        <Nav />
-      </MemoryRouter>
-    );
-    expect(screen.getByText("Home")).toHaveAttribute("href", "/");
-    expect(screen.getByText("Start Planning")).toHaveAttribute(
-      "href",
-      "/startplanning"
-    );
-    expect(screen.getByText("Sign In/Up")).toHaveAttribute("href", "/login");
-  });
+	it('links point to the correct location', () => {
+		render(
+			<MemoryRouter>
+				<Nav />
+			</MemoryRouter>
+		);
+		expect(screen.getByText('Home')).toHaveAttribute('href', '/');
+		expect(screen.getByText('Start Plan')).toHaveAttribute('href', '/startplanning');
+		expect(screen.getByText('Sign In/Up')).toHaveAttribute('href', '/login');
+	});
 });
