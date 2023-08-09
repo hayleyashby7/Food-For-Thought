1f12b0b9c82676b70f3d6fedce57499e081aad0e
Hayley Ashby <hayleyashby7@gmail.com>
2023-08-09 15:33:01 +0100

    refactor: :lipstick: Adjust header UI

--- client/src/components/layout/layout.tsx
+++ client/src/components/layout/layout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
 	return (
-		<div className='flex flex-col bg-yellow-100 min-h-screen w-screen'>
+		<div className='flex flex-col bg-yellow-100 min-h-screen w-screen min-w-fit'>
 			<Header />
 			<main className='px-4 py-6 sm:px-6 lg:px-8 flex-1'>
 				<Outlet />
